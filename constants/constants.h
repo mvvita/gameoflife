d259ea22a3004afc8d0f191cfcfe87a67a06e328
Marko <markovitiz7@gmail.com>
2019-05-17 16:14:38 +0200

    Added constants file

---
+++ constants/constants.h
@@ -0,0 +1,20 @@
+#ifndef CONSTANTS_HEADER
+#define CONSTANTS_HEADER
+
+/* Game Modes 
+    NORMAL - Classic game of life simulation
+    COEX - Coexistance, two species coexisting with each other
+    PREDATOR - Two species, one is predator (one kills the other)
+    VIRUS - Two species, one infects the other 
+    UNKNOWN - Two species, both evolve treating the other as dead cells
+ */
+
+typedef enum {NORMAL, COEX, PREDATOR, VIRUS, UNKNOWN} GAME_MODE;
+
+/* Cell Types 
+    Matching the above game modes. CELL_COEX_1 & CELL_COEX_2 used in game modes COEX & UNKNOWN. 
+ */
+
+typedef enum {CELL_NORMAL, CELL_COEX_1, CELL_COEX_2, CELL_PREDATOR, CELL_VIRUS} CELL_TYPE;
+
+#endif
